678fdbcebc270a356d8ab4bf3f8247de3600b78a
david8zhang <david8zhang@berkeley.edu>
2025-04-19 12:08:01 -0700

    added mouse to inventory grid position detection

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 @onready var mob = $Mob as Mob
 @onready var hero = $Hero as Hero
+@onready var camera = $Camera2D as Camera2D
 
 func _ready():
 	pass # Replace with function body.
--- scripts/Inventory.gd
+++ scripts/Inventory.gd
@@ -2,27 +2,42 @@ class_name Inventory
 extends VBoxContainer
 
 @export var inventory_box_scene: PackedScene
+@onready var game = get_node("/root/Main") as Game
+var top_left = Vector2(0, 0)
 
 const MAX_ROWS = 8
 const MAX_COLS = 8
 
 var num_rows = 4
 var num_cols = 4
-var padding = 10
+var padding = 5
 var inventory_box_size = 50
 
 func _ready():
-	var curr_x = 0
-	var curr_y = 0
-	
-	for i in range(0, MAX_ROWS):
+	var first_box
+	for i in range(0, num_rows):
 		var hbox_container = HBoxContainer.new()
-		curr_x = 0
-		for j in range(0, MAX_COLS):
-			var inventory_box = inventory_box_scene.instantiate()
+		for j in range(0, num_cols):
+			var inventory_box = inventory_box_scene.instantiate() as InventoryBox
 			hbox_container.add_child(inventory_box)
-			inventory_box.global_position = Vector2(curr_x, curr_y)
-			curr_x += inventory_box_size + padding
+			hbox_container.add_theme_constant_override("separation", 5)
+			if i == 0 and j == 0:
+				first_box = inventory_box
 		add_child(hbox_container)
 		hbox_container.alignment = BoxContainer.ALIGNMENT_CENTER
-		curr_y += inventory_box_size + padding
+	assert(first_box != null, "invalid initial inventory box!")
+	await get_tree().process_frame
+	top_left = first_box.get_global_transform().origin
+
+func _process(_delta):
+	var camera = game.camera as Camera2D
+	var mouse_pos = camera.get_global_mouse_position()
+	var board_pos = convert_world_pos_to_board_pos(mouse_pos)
+	print(board_pos)
+
+func convert_world_pos_to_board_pos(world_pos: Vector2):
+	var board_col = floor((world_pos.x - top_left.x) / (inventory_box_size + padding))
+	var board_row = floor((world_pos.y - top_left.y) / (inventory_box_size + padding))
+	if board_row >= 0 and board_row < num_rows and board_col >= 0 and board_col < num_cols:
+		return Vector2(board_row, board_col)
+	return Vector2(-1, -1)
